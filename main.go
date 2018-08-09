775c33ea3dfcfe533f1d4eda475e6d0995c34d4d
John Scherff <jscherff@24hourfit.com>
2018-08-08 18:17:44 -0700

    Changed year in license info

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2017 John Scherff
+// Copyright 2018 John Scherff
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
